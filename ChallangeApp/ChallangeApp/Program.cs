539377402cd293be507d92a8e04c32cd2251d9f2
Nazmeris <nazmeris0@gmail.com>
2023-07-12 14:37:39 +0200

    Zadanie domowe dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Zadanie dzień 4.
+var name = "Ewa";
+var sex = "F";
+var age = 67;
+
+if (age >= 30)
+{
+    if (sex == "M")
+    {
+        Console.WriteLine("Mężczyzna powyżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta powyżej 30 lat");
+    }
+}
+else
+{
+    if (sex == "M")
+    {
+        Console.WriteLine("Mężczyzna poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
+Console.WriteLine($"{name}, lat {age}.");
+if (age >= 18)
+{
+    if (sex == "M")
+    {
+        Console.WriteLine("Pełnotetni Mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Pełnoletnia Kobieta");
+    }
+}
+else
+{
+    if (sex == "M")
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Niepełnoletnia Kobieta");
+    }
+}
