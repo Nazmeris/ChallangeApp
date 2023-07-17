0465b0296866ee9f9dc2c31aca82353469452f5d
Nazmeris <nazmeris0@gmail.com>
2023-07-17 12:03:43 +0200

    Zadanie dzień 6!

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,71 +1,45 @@
-﻿long number = 57994139997;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
+﻿using ChallangeApp;
+using System.Reflection.Metadata;
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+Employee employee1 = new Employee("Adam", "Adamowicz", "40");
+Employee employee2 = new Employee("Ewa", "Adamowicz", "38");
+Employee employee3 = new Employee("Ada", "Adamowicz", "26");
 
-foreach (char letter in letters)
+employee1.AddScore(7);
+employee1.AddScore(8);
+employee1.AddScore(5);
+employee1.AddScore(4);
+employee1.AddScore(9);
+
+employee2.AddScore(9);
+employee2.AddScore(4);
+employee2.AddScore(1);
+employee2.AddScore(10);
+employee2.AddScore(10);
+
+employee3.AddScore(3);
+employee3.AddScore(5);
+employee3.AddScore(8);
+employee3.AddScore(5);
+employee3.AddScore(9);
+
+List<Employee> employees = new List<Employee>()
+{ 
+    employee1, employee2, employee3 
+};
+
+int maxResult = -1;
+Employee? employeeWithMaxResult = null;
+
+foreach (var employee in employees)
 {
-    if (letter == '0')
+    if (employee.Result > maxResult)
     {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
+        employeeWithMaxResult = employee;
+        maxResult = employee.Result;
     }
 }
-{
-    Console.WriteLine($"0  =  {counter0}");
-    Console.WriteLine($"1  =  {counter1}");
-    Console.WriteLine($"2  =  {counter2}");
-    Console.WriteLine($"3  =  {counter3}");
-    Console.WriteLine($"4  =  {counter4}");
-    Console.WriteLine($"5  =  {counter5}");
-    Console.WriteLine($"6  =  {counter6}");
-    Console.WriteLine($"7  =  {counter7}");
-    Console.WriteLine($"8  =  {counter8}");
-    Console.WriteLine($"9  =  {counter9}");
-    
-}
+
+
+Console.WriteLine("Najwyższą liczbę ocen otrzymała -" + " " + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + " " + employeeWithMaxResult.Age +
+                 " " + "lat z wynikiem" + " " + employeeWithMaxResult.Result + " " + "punktów.");
