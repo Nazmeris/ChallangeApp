ceca942ff53d557699d87f7b42b1953251daf328
Nazmeris <nazmeris0@gmail.com>
2023-07-26 15:25:44 +0200

    Zadanie Domowe dzień 10.

--- ChallangeApp/ChallangeApp/Employee.cs
+++ ChallangeApp/ChallangeApp/Employee.cs
@@ -10,12 +10,44 @@
         }
         public string Name { get; private set; }
         public string Surname { get; private set; }
-        
-        public void AddGrade(float grade) 
-        { 
-            this.grades.Add(grade);
-        }
 
+        public void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("invalid grade value");
+            }
+        }
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("String is not float");
+            }
+        }
+        public void AddGrade(int grade)
+        {
+            float result = (float)grade;
+            this.AddGrade((float)result);
+        }
+        public void AddGrade(long grade)
+        {
+            float result = (float)grade;
+            this.AddGrade((float)result);
+        }
+        public void AddGrade(double grade)
+        {
+            float result = (float)grade;
+            this.AddGrade((float)result);
+        }
         public Statistics GetStatistics()
         {
             var statistics = new Statistics();
--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -4,8 +4,10 @@ var employee = new Employee("John", "Shepard");
 
 Employee employee1 = new Employee("John", "Shepard");
 
+employee.AddGrade(7000);
+employee.AddGrade("Adam");
 employee.AddGrade(7);
-employee.AddGrade(4);
+employee.AddGrade("12");
 employee.AddGrade(7);
 
 var statistics = employee.GetStatistics();
@@ -15,4 +17,3 @@ Console.WriteLine($"Min:   {statistics.Min}");
 Console.WriteLine($"Max:   {statistics.Max}");
 Console.WriteLine($"Sum:   {statistics.Sum}");
 
-Console.WriteLine($"{employee1.Name} {employee1.Surname}");
