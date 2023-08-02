c803fb33d5a2faf071c2fb46e294d0d1fe6dc765
Nazmeris <nazmeris0@gmail.com>
2023-08-02 13:18:17 +0200

    Czysty projekt na dzień 12.

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,22 +1,9 @@
 ﻿using ChallangeApp;
 
-var employee = new Employee("John", "Shepard");
-
-Employee employee1 = new Employee("John", "Shepard");
-
+Employee employee = new Employee("John", "Shepard");
 employee.AddGrade(7000);
 employee.AddGrade("Adam");
 employee.AddGrade(7);
 employee.AddGrade("12");
 employee.AddGrade(7);
-
-var statistics = employee.GetStatistics();
-var statistics1 = employee.GetStatisticsWithFor();
-var statistics2 = employee.GetStatisticsWithDoWhile();
-var statistics3 = employee.GetStatisticsWithWhile();
-
-Console.WriteLine($"Averge:   { statistics.Avarge:N2}");
-Console.WriteLine($"Min:   {statistics.Min}");
-Console.WriteLine($"Max:   {statistics.Max}");
-Console.WriteLine($"Sum:   {statistics.Sum}");
 
